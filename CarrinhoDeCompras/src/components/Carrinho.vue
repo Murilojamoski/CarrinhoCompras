4840af7bdf1fa6213d19deaadea46731d767ba4a
MuriloJamoski <mugames2002@gmail.com>
2025-04-15 10:25:17 -0300

    FEAT: funcionalade Carrinho

---
+++ CarrinhoDeCompras/src/components/Carrinho.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { carrinho } from './carrinho'
+</script>
+
+<template>
+  <div class="Carrinho">
+    <h1>Itens no carrinho</h1>
+    <div v-if="carrinho.length === 0">
+      <p>Nenhum item adicionado ainda.</p>
+    </div>
+    <div v-else>
+      <div v-for="livro in carrinho" :key="livro.titulo">
+        <p>{{ livro.titulo }} - R$ {{ livro.preco }}</p>
+      </div>
+    </div>
+  </div>
+</template>
