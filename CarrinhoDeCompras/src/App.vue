3e335e004ad5c06b93347b3891a6a3c760773475
MuriloJamoski <mugames2002@gmail.com>
2025-04-15 09:36:46 -0300

    FEAT: componemtes Nav e Corpos

---
+++ CarrinhoDeCompras/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Navegacao from './components/Navegacao.vue';
+import Corpo from './components/Corpo.vue';
+</script>
+
+<template>
+
+ <Navegacao />
+  <Corpo />
+</template>
+
+<style scoped>
+
+</style>
